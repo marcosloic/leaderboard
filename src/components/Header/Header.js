fd5e3810bbe6df734588f96d80133ffb0f2ebb40
Hao Dong <dong_hao@dazn.com>
2018-11-20 17:02:38 +0000

    Added filtering and buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,21 @@ class Header extends Component {
     render() {
         return (
             <header>
-                <img alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/7/71/DAZN_logo.svg" />
-                <h1>Leaderboard</h1>
+                <div className="branding">
+                    <img alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/7/71/DAZN_logo.svg" />
+                    <h1>Leaderboard</h1>
+                </div>
+                <div className="controls">
+                    <label>Filter by:</label>
+                    <select>
+                        <option>Football</option>
+                        <option>Basket Ball</option>
+                        <option>Rugby</option>
+                    </select>
+                    <button>This Week</button>
+                    <button>This Month</button>
+                    <button>All Time</button>
+                </div>
             </header>
         )
     }
